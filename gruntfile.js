31fcc96bc3a05d65262926b435f46b874a95770f
110chang <u@110chang.com>
2015-12-14 20:03:30 +0900

    first-commit

---
+++ gruntfile.js
@@ -0,0 +1,35 @@
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    browserify: {
+      dist: {
+        options: {
+          transform: [
+            ["babelify", {presets: ['es2015']}]
+          ]
+        },
+        files: {
+          // if the source file has an extension of es6 then
+          // we change the name of the source file accordingly.
+          // The result file's extension is always .js
+          "./public/js/app.js": ["./src/index.es6"]
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ["./src/**/*.es6"],
+        tasks: ["browserify"]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-browserify");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+  grunt.registerTask("default", ["watch"]);
+  grunt.registerTask("build", ["browserify"]);
+};
+
+//babelify v7.2.* で 'sourceType: module' エラーを回避
+//http://qiita.com/do7be/items/521dd4e61a237e885818
